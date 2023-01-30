0b76497f870715428f1268bb2dcc268ca63215e7
swathikrishnaus <swathikrishnaus2015@gmail.com>
2023-01-31 01:55:57 +0530

    added

--- js/script.js
+++ js/script.js
@@ -22,3 +22,16 @@
    	 });
    }
  });
+
+
+//  -------------------toggle-----------------------
+
+$('#toggle').click(function() {
+	$(this).toggleClass('active');
+	$('#overlay').toggleClass('open');
+  });
+  $('.overlay-menu a').click(function() {
+	$('.button_container').toggleClass('active');
+	$('#overlay').toggleClass('open');
+	return false;
+  });
